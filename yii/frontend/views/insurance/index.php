f6749b09f2f8493a853e18e10a7dfbfc5a3ac520
shatharababah22 <shatha.rababah22@gmail.com>
2024-08-02 01:03:15 +0300

    shatha

--- yii/frontend/views/insurance/index.php
+++ yii/frontend/views/insurance/index.php
@@ -248,8 +248,29 @@ $this->title = 'Plan Details';
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-check-circle-fill text-success" viewBox="0 0 16 16">
                                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
                                                         </svg>
-                                                    </span>
-                                                    <span class="ms-2"><?= $planCoverage->description ?></span>
+                                                        <?php
+$description = $planCoverage->description;
+$maxLength =28; 
+
+if (strlen($description) > $maxLength) {
+    $splitPoint = strpos($description, ' ', $maxLength);
+    if ($splitPoint === false) {
+        $splitPoint = $maxLength;
+    }
+    $firstLine = substr($description, 0, $splitPoint);
+    $secondLine = substr($description, $splitPoint);
+} else {
+    $firstLine = $description;
+    $secondLine = '';
+}
+?>
+<span class="ms-2">
+    <?= htmlspecialchars($firstLine) ?><br>
+  <span class="ms-4"><?= htmlspecialchars($secondLine) ?></span>  
+</span>
+
+
+
                                                 <?php else : ?>
                                                     <span>
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-x-circle-fill text-danger" viewBox="0 0 16 16">
